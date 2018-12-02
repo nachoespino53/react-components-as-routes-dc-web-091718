4cc86f662dc3b4b93482cf9e6b67489424480ad4
nachoespino53 <nachoespino53@gmail.com>
2018-12-02 15:39:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
 
-ReactDOM.render(
-  <Home />,
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+ 
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Route path='/' component = {Home} />
+      <Route exact path='/login' component = {Login} />
+      <Route exact path='/about' component = {About} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
